434c0a65d417db8ba1cd942aaeb95502e70b99f7
MaximeArn <maxime-arnould@outlook.fr>
2020-12-03 00:59:59 +0100

    add buttons to landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -21,9 +21,17 @@ const Landing = () => {
                     </div>
                 </header>
             <div className="landing-content">
-                <p className="presentation">Hi Myself <strong>Maxime Arnould</strong></p>
-                <h2 className="sub-title">Professionel Web Developer</h2>
-                <p className="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi dolorum perspiciatis ipsa quos quam. Vero reiciendis quasi pariatur obcaecati animi totam architecto corporis accusamus beatae, cumque rerum unde labore aliquid!</p>
+
+                <div className="text">
+                    <p className="presentation">Hi Myself <strong>Maxime Arnould</strong></p>
+                    <h2 className="sub-title">Professionel Web Developer</h2>
+                    <p className="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi dolorum perspiciatis ipsa quos quam. Vero reiciendis quasi pariatur obcaecati animi totam architecto corporis accusamus beatae, cumque rerum unde labore aliquid!</p>
+                </div>
+
+                <div className="buttons">
+                    <button className="green-button">About Me</button>
+                    <button>Download CV</button>
+                </div>
             </div>
         </div>
     )
