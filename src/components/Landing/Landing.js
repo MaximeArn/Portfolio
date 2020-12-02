cf703b45004dae85fa7783046dde68e0de4d17bb
MaximeArn <maxime-arnould@outlook.fr>
2020-12-03 00:45:00 +0100

    landing page texts and css

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './landing.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars} from '@fortawesome/free-solid-svg-icons'
 
 const Landing = () => {
     return (
-        <div className="landing-background"></div>
+        <div className="landing-background">
+                <header className="navbar">
+                    <div className="logo-section">
+                        <div className="logo">
+                            <p>M</p>
+                        </div>
+                        <div className="logo-text">
+                            <h1>Maxime Arnould</h1>
+                            <h2>Web Developer</h2>
+                        </div>
+                    </div>
+                    <div className="burger-icon">
+                        <FontAwesomeIcon icon={faBars} />
+                    </div>
+                </header>
+            <div className="landing-content">
+                <p className="presentation">Hi Myself <strong>Maxime Arnould</strong></p>
+                <h2 className="sub-title">Professionel Web Developer</h2>
+                <p className="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi dolorum perspiciatis ipsa quos quam. Vero reiciendis quasi pariatur obcaecati animi totam architecto corporis accusamus beatae, cumque rerum unde labore aliquid!</p>
+            </div>
+        </div>
     )
 }
 
