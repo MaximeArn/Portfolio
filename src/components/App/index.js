5c102167ce0b5400e7c5592a587ac5628b6e5122
MaximeArn <maxime-arnould@outlook.fr>
2020-12-02 18:17:32 +0100

    add mockup and images

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,10 @@
-// == Import npm
 import React from 'react';
+import Landing from '../Landing/Landing'; 
 
-// == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
-
-// == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <Landing />
   </div>
 );
 
-// == Export
 export default App;
