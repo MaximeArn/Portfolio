6baedee01c4567b1eb73040056768b8569ebb346
MaximeArn <maxime-arnould@outlook.fr>
2020-12-10 23:28:29 +0100

    3 projects/row on desktop

--- src/data/projects.js
+++ src/data/projects.js
@@ -24,4 +24,25 @@ export default [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur possimus amet architecto nam iste veritatis, culpa beatae! Illo nisi dolores ducimus dolore assumenda quam, cumque fuga magnam, ab voluptate odit.",
   },
+  {
+    name: "calculator",
+    technos: ["scss", "javascript", "react"],
+    image: calculator,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur possimus amet architecto nam iste veritatis, culpa beatae! Illo nisi dolores ducimus dolore assumenda quam, cumque fuga magnam, ab voluptate odit.",
+  },
+  {
+    name: "tindev",
+    technos: ["typescript", "react", "redux", "mongoose"],
+    image: tindev,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur possimus amet architecto nam iste veritatis, culpa beatae! Illo nisi dolores ducimus dolore assumenda quam, cumque fuga magnam, ab voluptate odit.",
+  },
+  {
+    name: "portfolio",
+    technos: ["scss", "javascript", "react"],
+    image: portfolio,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur possimus amet architecto nam iste veritatis, culpa beatae! Illo nisi dolores ducimus dolore assumenda quam, cumque fuga magnam, ab voluptate odit.",
+  },
 ];
